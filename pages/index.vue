23e3cb1dd5918aaa26d9582b979dde1e2c05c9ba
luoxn@zhujia100.com <luoxn339@qq.com>
2018-10-23 10:04:21 +0800

    test

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div :class="style.test">
+    test
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style module>
+.test {
+  background: red;
+}
+</style>
